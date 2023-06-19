5779999bdb27a371c119f619888e617863695e17
Manish Kumar <mkmanishkumar.rai@gmail.com>
2023-06-19 13:09:41 +0530

    updated

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/',(req,res)=>{
     res.send(`Server is runnning on ${port}`)
 })
 
+app.post('/add',(req,res)=>{
+    res.send({message : "Added item"})
+})
+
+
 
 app.listen(port , (req,res)=>{
     console.log(`server is running on post ${port}`);
